763c56470ab810b5c51d70889da82eaaf78f84ee
saulocapistrano <saulocapistrano@gmail.com>
2025-01-30 14:07:39 -0300

    fix: ensure TransactionRequestDTO has proper constructors and getters

--- src/main/java/com/itub/itub3/application/dto/request/TransactionRequestDTO.java
+++ src/main/java/com/itub/itub3/application/dto/request/TransactionRequestDTO.java
@@ -5,11 +5,13 @@ import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class TransactionRequestDTO {
@@ -18,5 +20,12 @@ public class TransactionRequestDTO {
     private BigDecimal value;
 
     @NotNull(message = "Date and time is required")
-    private OffsetDateTime dateTimeStamp;
+    private OffsetDateTime dateTime;
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public OffsetDateTime getDateTime() {
+        return dateTime;
+    }
 }
