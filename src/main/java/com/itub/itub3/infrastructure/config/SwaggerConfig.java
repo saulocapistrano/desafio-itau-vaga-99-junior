b1d921aa2ada636f458cd7dec15f1ced0fd2dbcc
saulocapistrano <saulocapistrano@gmail.com>
2025-01-30 14:08:52 -0300

    feat: add Swagger configuration for API documentation

---
+++ src/main/java/com/itub/itub3/infrastructure/config/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package com.itub.itub3.infrastructure.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Transaction API")
+                        .version("1.0")
+                        .description("API for handling transactions and statistics"));
+    }
+}
