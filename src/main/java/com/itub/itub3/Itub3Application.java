383ca37be9df3526ae86f9591e838022042a11fb
saulocapistrano <saulocapistrano@gmail.com>
2025-01-30 09:08:19 -0300

    Init: create project

---
+++ src/main/java/com/itub/itub3/Itub3Application.java
@@ -0,0 +1,13 @@
+package com.itub.itub3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Itub3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Itub3Application.class, args);
+    }
+
+}
