6b3d4d24cf9a2221504e9066e5f68be72a615965
saulocapistrano <saulocapistrano@gmail.com>
2025-01-30 11:42:52 -0300

    feat: implement deleteTransactions and getStatistics in TransactionController

--- src/main/java/com/itub/itub3/interfaces/controller/TransactionController.java
+++ src/main/java/com/itub/itub3/interfaces/controller/TransactionController.java
@@ -1,6 +1,7 @@
 package com.itub.itub3.interfaces.controller;
 
 import com.itub.itub3.application.dto.request.TransactionRequestDTO;
+import com.itub.itub3.application.dto.response.StatisticsResponseDTO;
 import com.itub.itub3.application.service.TransactionService;
 import com.itub.itub3.interfaces.controller.api.TransactionApi;
 import org.springframework.http.HttpStatus;
@@ -19,6 +20,18 @@ public class TransactionController implements TransactionApi {
     public ResponseEntity<Void> createTransaction(TransactionRequestDTO transactionRequestDTO) {
         transactionService.registerTransaction(transactionRequestDTO);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @Override
+    public ResponseEntity<Void> deleteTransactions() {
+        transactionService.deleteTransaction();
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @Override
+    public ResponseEntity<StatisticsResponseDTO> getStatistics() {
+        StatisticsResponseDTO statisticsResponseDTO = transactionService.getStatistics();
+        return ResponseEntity.ok(statisticsResponseDTO);
     }
 
 }
