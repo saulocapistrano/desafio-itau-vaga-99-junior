2efa4da17536bdea1b7b145e240238aa0206f685
saulocapistrano <saulocapistrano@gmail.com>
2025-01-30 11:42:22 -0300

    feat: add delete and statistics endpoints in TransactionApi

--- src/main/java/com/itub/itub3/interfaces/controller/api/TransactionApi.java
+++ src/main/java/com/itub/itub3/interfaces/controller/api/TransactionApi.java
@@ -1,14 +1,19 @@
 package com.itub.itub3.interfaces.controller.api;
 
 import com.itub.itub3.application.dto.request.TransactionRequestDTO;
+import com.itub.itub3.application.dto.response.StatisticsResponseDTO;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/transactions")
 public interface TransactionApi {
     @PostMapping
     ResponseEntity<Void> createTransaction(@Valid @RequestBody TransactionRequestDTO requestDTO);
+
+    @DeleteMapping
+    ResponseEntity<Void> deleteTransactions();
+
+    @GetMapping("/statistics")
+    ResponseEntity<StatisticsResponseDTO> getStatistics();
 }
