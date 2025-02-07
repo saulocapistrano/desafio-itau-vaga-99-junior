d0b3bc8205aa82c4e5c6b8067c28d8751cca47e4
saulocapistrano <saulocapistrano@gmail.com>
2025-02-07 08:33:40 -0300

    feat: Improve transaction request DTO validation

--- src/main/java/com/itub/itub3/application/dto/request/TransactionRequestDTO.java
+++ src/main/java/com/itub/itub3/application/dto/request/TransactionRequestDTO.java
@@ -3,29 +3,40 @@ package com.itub.itub3.application.dto.request;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class TransactionRequestDTO {
+
     @NotNull
     @DecimalMin(value = "0.0", inclusive = true, message = "Needed value must be equal or greater than 0")
     private BigDecimal value;
 
     @NotNull(message = "Date and time is required")
     private OffsetDateTime dateTime;
+
     public BigDecimal getValue() {
         return value;
     }
 
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+
     public OffsetDateTime getDateTime() {
         return dateTime;
+    }
+
+    public void setDateTime(OffsetDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public TransactionRequestDTO() {
+    }
+
+    public TransactionRequestDTO(BigDecimal value, OffsetDateTime dateTime) {
+        this.value = value;
+        this.dateTime = dateTime;
     }
 }
