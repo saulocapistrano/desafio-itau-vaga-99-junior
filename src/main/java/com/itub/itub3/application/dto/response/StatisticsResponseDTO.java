7d5f824cd23fe78378fee62e5c103c61966b71d5
saulocapistrano <saulocapistrano@gmail.com>
2025-02-07 08:33:24 -0300

    feat: Enhance statistics response DTO for better data representation

--- src/main/java/com/itub/itub3/application/dto/response/StatisticsResponseDTO.java
+++ src/main/java/com/itub/itub3/application/dto/response/StatisticsResponseDTO.java
@@ -3,7 +3,6 @@ package com.itub.itub3.application.dto.response;
 import lombok.Getter;
 
 import java.math.BigDecimal;
-@Getter
 public class StatisticsResponseDTO {
     private long count;
     private BigDecimal sum;
@@ -11,6 +10,35 @@ public class StatisticsResponseDTO {
     private BigDecimal min;
     private BigDecimal max;
 
-    public StatisticsResponseDTO(int i, BigDecimal zero, BigDecimal zero1, BigDecimal zero2, BigDecimal zero3) {
+    public StatisticsResponseDTO(long count, BigDecimal sum, BigDecimal avg, BigDecimal min, BigDecimal max) {
+        this.count = count;
+        this.sum = sum;
+        this.avg = avg;
+        this.min = min;
+        this.max = max;
+    }
+
+
+    public StatisticsResponseDTO() {
+    }
+
+    public long getCount() {
+        return count;
+    }
+
+    public BigDecimal getSum() {
+        return sum;
+    }
+
+    public BigDecimal getAvg() {
+        return avg;
+    }
+
+    public BigDecimal getMin() {
+        return min;
+    }
+
+    public BigDecimal getMax() {
+        return max;
     }
 }
